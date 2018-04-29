fa6896dcc1a11e519dbdf09663ccdebf47e623d5
Iurii Milovanov <duruku@gmail.com>
2018-04-29 17:57:39 -0500

    added CPU allocation and restart policy for standalone deployment

--- dockerRun.sh
+++ dockerRun.sh
@@ -4,9 +4,10 @@ APP_PORT=$2
 
 MEMORY="2g"
 SWAP_MEMORY="1g"
+CPUS="4"
 
-if [ $(docker inspect -f '{{.State.Running}}' $APP_NAMR) = "true" ]; then
-  docker kill "${APP_NAME}"
-fi
+docker kill "${APP_NAME}" || true
+docker rm -f "${APP_NAME}" || true
 
-docker run --rm -d --memory="${MEMORY}" --memory-swap="${SWAP_MEMORY}" --name "${APP_NAME}" -p "${APP_PORT}:3838" "${APP_NAME}"
+docker run -d --cpus="${CPUS}" --memory="${MEMORY}" --memory-swap="${SWAP_MEMORY}" --name "${APP_NAME}" -p "${APP_PORT}:3838" "${APP_NAME}"
+docker update --restart always "${APP_NAME}"
